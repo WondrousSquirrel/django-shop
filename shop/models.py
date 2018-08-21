598a61f259c7e8bb3df5ad36da0bf41370f3f6ff
Alexander <wondroussquirrel@gmail.com>
2018-08-21 17:02:04 +0300

    create view with bootstrap

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
@@ -15,6 +15,9 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
     
 
 class Product(models.Model):
@@ -40,3 +43,6 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
---
+++ shop/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from shop.views import (
+    product_list, 
+    product_detail
+)
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('', product_list, name='product_list'),
+    path('<str:category_slug>/', product_list, name='product_list_by_category'),
+    path('<int:id>/<str:slug>/', product_detail, name='product_detail')
+]
