ae00fd7a83b40e5eac012de6b85733c51520b983
Alexander <wondroussquirrel@gmail.com>
2018-08-22 00:22:03 +0300

    shoping cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,4 +1,4 @@
-'''from django.urls import path
+from django.urls import path
 
 from . views import (
     cart_add, 
@@ -9,9 +9,9 @@ from . views import (
 app_name = 'cart'
 
 urlpatterns = [
-    path('', cart_detail, name='cart-detail'),
-    path('add/<int:product_id>/', cart_add, name='cart-add'),
-    path('remove/<int:product_id>/', cart_remove, name='cart-remove'),
+    path('', cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', cart_add, name='cart_add'),
+    path('remove/<int:product_id>/', cart_remove, name='cart_remove'),
 ]
 '''
 from django.conf.urls import url
@@ -24,3 +24,4 @@ urlpatterns = [
     url(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='cart_add'),
     url(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='cart_remove'),
 ]
+'''
